b7cf4a546739523996059290868510f9cdaef95b
driverou <95316958+driverou@users.noreply.github.com>
2023-10-29 15:09:36 -0400

    Modified visuals on the plotting. Added a new function to plot the best fit polynomial as well

--- ImageToPolynomial.py
+++ ImageToPolynomial.py
@@ -1,12 +1,18 @@
 import cv2
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
-import matplotlib.pyplot as plt  # For visualization
-from mpl_toolkits.mplot3d import Axes3D  # Import the 3D plotting module
 
-def plot_3d(img_smooth):
+# Set Seaborn's default aesthetics
+sns.set()
+sns.set_style("white")
+
+def plot_3d_image(img_smooth):
+    
     # Get the dimensions of the image
     h, w = img_smooth.shape
 
@@ -19,19 +25,48 @@ def plot_3d(img_smooth):
     # Create 3D axes
     ax = fig.add_subplot(111, projection='3d')
 
-    # Plot the surface
-    ax.plot_surface(x, y, img_smooth, cmap='gray_r', linewidth=0, antialiased=False)
+    # Plot the surface for smoothed image
+    surf = ax.plot_surface(x, y, img_smooth, cmap='coolwarm', linewidth=0, antialiased=True, alpha=0.7)
 
     # Set labels and title
-    ax.set_xlabel('X-axis')
-    ax.set_ylabel('Y-axis')
-    ax.set_zlabel('Pixel Value')
-    ax.set_title('3D Plot of Image')
+    ax.set_xlabel('X-axis', labelpad=10)
+    ax.set_ylabel('Y-axis', labelpad=10)
+    ax.set_zlabel('Pixel Value', labelpad=10)
+    ax.set_title('3D Plot of Smoothed Image', pad=20)
 
+    # Add a colorbar
+    fig.colorbar(surf, ax=ax, shrink=0.5, aspect=10, pad=0.1, label='Smoothed Image Value')
+    ax.grid(False)  # Hide gridlines
     plt.show()
+def plot_3d_polynomial(model, h, w):
+    # Create arrays of x, y coordinates
+    y, x = np.mgrid[:h, :w]
 
+    # Create a grid of coordinates covering the image
+    coordinates = np.hstack((x.flatten().reshape(-1, 1), y.flatten().reshape(-1, 1)))
 
+    # Evaluate the polynomial at each point in the grid
+    z_fit = model.predict(coordinates).reshape(h, w)
 
+    # Create a figure
+    fig = plt.figure(figsize=(10, 8))
+
+    # Create 3D axes
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot the surface for fitted polynomial
+    surf = ax.plot_surface(x, y, z_fit, cmap='viridis', linewidth=0, antialiased=True, alpha=0.7)
+
+    # Set labels and title
+    ax.set_xlabel('X-axis', labelpad=10)
+    ax.set_ylabel('Y-axis', labelpad=10)
+    ax.set_zlabel('Pixel Value', labelpad=10)
+    ax.set_title('3D Plot of Fitted Polynomial', pad=20)
+
+    # Add a colorbar
+    fig.colorbar(surf, ax=ax, shrink=0.5, aspect=10, pad=0.1, label='Fitted Polynomial Value')
+    ax.grid(False)  # Hide gridlines
+    plt.show()
 def image_to_polynomial(image_path, degree=2, blur_size=5, blur_sigma=1.5):
     # Load and preprocess the image
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -87,30 +122,37 @@ def visualize_topology(image_path, model, img_smooth, degree, blur_size, blur_si
     # Evaluate the polynomial at each point in the grid
     z_fit = model.predict(coordinates).reshape(h, w)
 
-    # Display the original and fitted topologies
     plt.figure(figsize=(15, 5))
+
     plt.subplot(1, 3, 1)
     plt.imshow(cv2.imread(image_path, cv2.IMREAD_GRAYSCALE), cmap='gray', origin='upper')
     plt.title('Original Topology')
-    plt.axis('on')  # Hide axes
+    plt.axis('off')  # Hide axes
+
     plt.subplot(1, 3, 2)
-    plt.imshow(img_smooth, cmap='gray', origin='upper')  # Display the smoothed image
-    plt.title(f'Smoothed Topology\nBlur Size: {blur_size}, Blur Sigma: {blur_sigma}')  # Display blur parameters
-    plt.axis('on')  # Hide axes
+    plt.imshow(img_smooth, cmap='gray', origin='upper')
+    plt.title(f'Smoothed Topology\nBlur Size: {blur_size}, Blur Sigma: {blur_sigma}')
+    plt.axis('off')  # Hide axes
+
     plt.subplot(1, 3, 3)
-    im = plt.imshow(z_fit, cmap='viridis', origin='upper')  # Use a colorful colormap
-    plt.title(f'Fitted Topology\nPolynomial Degree: {degree}')  # Display polynomial degree
-    plt.axis('on')  # Hide axes
-    plt.colorbar(im, label='Fitted Value')  # Add a colorbar with label
-    plt.tight_layout()  # Adjust spacing between subplots
+    im = plt.imshow(z_fit, cmap='viridis', origin='upper')
+    plt.title(f'Fitted Topology\nPolynomial Degree: {degree}')
+    plt.axis('off')  # Hide axes
+    cbar = plt.colorbar(im, label='Fitted Value')
+    cbar.ax.yaxis.set_tick_params(pad=15)  # Adjust colorbar tick label padding
+
+    plt.tight_layout()
     plt.show()
 
 
+
 # Usage:
-image_path = '/Users/diegorivero/Downloads/Untitled_Artwork 3.jpg'
+image_path = '/Users/diegorivero/Downloads/Untitled_Artwork 4.jpg'
 degree = 7
 blur_size = 11
 blur_sigma = 5
 model, img_smooth = image_to_polynomial(image_path, degree, blur_size, blur_sigma)
 visualize_topology(image_path, model, img_smooth, degree, blur_size, blur_sigma)
-plot_3d(img_smooth)
+h, w = img_smooth.shape
+plot_3d_image(img_smooth)
+plot_3d_polynomial(model, h, w)
